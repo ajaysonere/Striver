bdae0dcfda8a8c04518e5b8ce4866e0775500f28
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-05 09:05:50 +0530

    count digit

--- 01. Basics/02. Basic Math/countDigit.cpp
+++ 01. Basics/02. Basic Math/countDigit.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int countEvenlyDigit(int n){
+    int digit = n;
+    int count = 0;
+    while(n>0){
+        int temp = n%10;
+        if(temp != 0){
+          if(digit%temp == 0){
+               count++;
+           }
+        } 
+        n = n/10;
+    }
+    cout << count << "\n";
+    return count;
+}
+
 int main ()
 {
     cout << "Count Number Of Digits "<< "\n";
+    int n;
+    cin >> n;
+
+    int ans = countEvenlyDigit(n);
+    cout << ans << "\n";
     return 0;
 }
