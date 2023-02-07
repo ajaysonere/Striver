f1be8994eacdbae22a9d05d2c2af434a1030c5bf
Ajay Sonere <ajaysonere786@gmail.com>
2023-02-07 23:10:56 +0530

    diviser

--- 01. Basics/02. Basic Math/printAllDevicers.cpp
+++ 01. Basics/02. Basic Math/printAllDevicers.cpp
@@ -6,10 +6,12 @@ int main ()
  int n;
  cin >> n;
 
+ long long sum  =0;
  for(int i=1; i<=n; i++){
-    if(n%i==0){
-        cout << i << " ";
-    }
+        int mod = n/i;
+        sum += mod*i;
  }
+
+ cout << sum << "\n";
  return 0;
 }
